498362ce83b492026c775afa371a0938abebb72c
Camilo López <camiloj.lopez79@gmail.com>
2022-12-10 01:57:51 -0300

    Corrección Variables y constantes

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Variables y Constantes
+
+const nombre = 'Camilo';
+const apellido = 'López';
+
+let valorDado = 6;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+if ( true ) {
+  const nombre = 'Javier';
+  console.log( nombre );
+}
+
